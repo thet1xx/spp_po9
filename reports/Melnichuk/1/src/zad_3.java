afe9f3b31beb8765632df9991ef51498a187cbfa
nikalow1 <42293743+nikalow1@users.noreply.github.com>
2024-02-12 12:45:34 +0300

    task_01

---
+++ reports/Melnichuk/1/src/zad_3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class zad_3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String line = scanner.nextLine();
+        System.out.println("На сколько сместить:");
+        int shift = scanner.nextInt();
+        System.out.println(zad_3.shiftRight(line, shift));
+
+    }
+    public static String shiftRight(String srt, int shift){
+        int cursor;
+        if(shift < 0){
+            cursor = -shift;
+        }else {
+            cursor = srt.length() - shift;
+        }
+        return srt.substring(cursor) + srt.substring(0, cursor);
+    }
+}
