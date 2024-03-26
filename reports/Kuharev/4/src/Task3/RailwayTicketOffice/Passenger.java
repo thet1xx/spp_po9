7032ad1297d969d9762b72c7362f9ce341428f57
DKN <82839024+Ian3707@users.noreply.github.com>
2024-03-26 13:43:17 +0300

    commit

---
+++ reports/Kuharev/4/src/Task3/RailwayTicketOffice/Passenger.java
@@ -0,0 +1,40 @@
+package RailwayTicketOffice;
+
+import RailwayTicketOffice.Person;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+import java.util.concurrent.ScheduledFuture;
+
+public class Passenger extends Person {
+    public Passenger(){
+        type = Const.USER;
+    }
+    public void Action(){
+        System.out.println("\tUser");
+        Scanner read = new Scanner(System.in);
+        String action = "";
+        while(!action.equals("quit")){
+            action = read.next();
+            Menu(action);
+        }
+    }
+    public void Menu(String action){//реализация
+        switch (action){
+            case "quit":
+                return;
+            case "1":
+                RailwayOffice.showSchedule();
+                break;
+            case "2":
+                RailwayOffice.findsuitableTrain();
+                break;
+            case "3":
+                RailwayOffice.chooseTicket(RailwayOffice.findsuitableTrain(), this);
+                break;
+            default:
+                System.out.println("1 - Show schedule\n2 - Find suitable trains\n3 - Make request\n%any% - help\nquit to exit");
+                break;
+        }
+    }
+}
