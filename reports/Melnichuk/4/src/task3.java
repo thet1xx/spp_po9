714a4177fc7103278f6a855923303e1feb30c530
nikalow1 <42293743+nikalow1@users.noreply.github.com>
2024-04-10 14:55:51 +0300

    fix task&rep

--- reports/Melnichuk/4/src/task3.java
+++ reports/Melnichuk/4/src/task3.java
@@ -5,17 +5,18 @@ class Elective{
     private String name;
     private Teacher teacher;
     private List<Student> students;
-    private Course course;
 
-    public Elective(String name, Teacher teacher, Course course){
+    public Elective(String name, Teacher teacher){
         this.name = name;
         this.teacher = teacher;
-        this.course = course;
         students = new ArrayList<>();
     }
 
     public void registration(Student student){
         students.add(student);
+    }
+    public String getName(){
+        return name;
     }
 
     public void learning(){
@@ -34,12 +35,12 @@ class Teacher{
     public Teacher(String name){
         this.name = name;
     }
-    public void advertReg(Course course){
-        System.out.println("Запись на курс \"" + course.getName() + "\" объявлена преподавателем " + name + ".");
+    public void advertReg(Elective elective){
+        System.out.println("Запись на курс \"" + elective.getName() + "\" объявлена преподавателем " + name + ".");
     }
 
-    public void setMark(Student student, int mark){
-        Archive.saveMark(student, mark);
+    public void setMark(Student student, int mark, Elective elective){
+        Archive.saveMark(student, mark, elective);
         System.out.println("Оценка " + mark + " выставлена " + student.getName());
     }
 }
@@ -53,8 +54,9 @@ class Student{
         this.name = name;
     }
 
-    public void registration(Course course){
-        System.out.println("Студент " + name + " записался на курс " + Course.getName());
+    public void registration(Elective elective){
+        elective.registration(this);
+        System.out.println("Студент " + name + " записался на курс " + elective.getName());
     }
 
     public String getName(){
@@ -62,25 +64,14 @@ class Student{
     }
 }
 
-class Course {
-    private static String name;
-
-    public Course(String name){
-        this.name = name;
-    }
-    public static String getName(){
-        return name;
-    }
-}
-
 class Archive{
     private static List<String> marks;
 
-    static {
+    public Archive() {
         marks = new ArrayList<>();
     }
-    public static void saveMark(Student student, int mark){
-        marks.add("Студент " + student.getName() + ": Оценка " + mark);
+    public static void saveMark(Student student, int mark, Elective elective){
+        marks.add("Студент: " + student.getName() + ", Курс: " + elective.getName() +  ", Оценка: " + mark);
     }
 
     public List<String> getMarks(){
@@ -89,31 +80,40 @@ class Archive{
 }
 public class task3{
     public static void main(String[] args){
-         Course course = new Course("Математика");
-         Archive marks = new Archive();
-
          Teacher teacher = new Teacher("Иванченко А.В.");
-         teacher.advertReg(course);
+         Teacher teacher1 = new Teacher("Василенко В.Д.");
 
-         Elective elective = new Elective("Факультатив по математике", teacher, course);
+         Elective elective = new Elective("Математика", teacher);
+         Elective elective1 = new Elective("Физика", teacher1);
+         Archive archive = new Archive();
+
+         teacher.advertReg(elective);
+         teacher1.advertReg(elective1);
 
          Student student1 = new Student(210663, "Melenkov");
          Student student2 = new Student(210664, "Molankova");
 
-         elective.registration(student1);
-         elective.registration(student2);
+         student1.registration(elective);
+         student2.registration(elective);
 
-         student1.registration(course);
-         student2.registration(course);
+         student1.registration(elective1);
+         student2.registration(elective1);
 
          elective.learning();
+         elective1.learning();
          elective.finish();
-         teacher.setMark(student1, 4);
-         teacher.setMark(student2, 5);
-        List<> archives = marks.getMarks();
-         for(Object archive : archives){
-             System.out.println();
-         }
+         elective1.finish();
 
+         teacher.setMark(student1, 4, elective);
+         teacher1.setMark(student1, 2, elective1);
+
+         teacher.setMark(student2, 5, elective);
+         teacher1.setMark(student2, 3, elective1);
+
+         List<String> marks = archive.getMarks();
+         for(String mark : marks){
+             System.out.println(mark);
+         }
     }
 }
+
