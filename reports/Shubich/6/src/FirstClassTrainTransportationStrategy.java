119d4dbf431da5d651d4565a1009ae8e45e98675
Darya Shubich <Dasha.hej@mail.ru>
2024-04-24 17:18:08 +0300

    update task01, task03

--- reports/Shubich/6/src/FirstClassTrainTransportationStrategy.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class FirstClassTrainTransportationStrategy implements TourPackageStrategy{
-    @Override
-    public int calculateCost() {
-        return 150;  // Пример стоимости транспорта первого класса
-    }
-}
--- reports/Shubich/6/src/LuxuryHotelAccommodationStrategy.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class LuxuryHotelAccommodationStrategy implements TourPackageStrategy {
-        @Override
-        public int calculateCost() {
-            return 300;  // Пример стоимости проживания в отеле высокого класса
-        }
-}
